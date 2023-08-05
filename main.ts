da7a82458cb18f523d9f750c8c237c694363b3b4
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2023-08-05 12:40:15 -0400

    added comments to main.ts

--- main.ts
+++ main.ts
@@ -13,9 +13,10 @@ import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 import * as mod from "https://deno.land/std@0.197.0/flags/mod.ts";
 import {load} from "https://deno.land/std@0.197.0/dotenv/mod.ts";
+
 await load()
 
-
+// Getting environment variables for API use
 const planet_key = Deno.env.get('PLANET_KEY')
 export default planet_key
 
