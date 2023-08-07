981f121f305851268d8b197f1dc52cb555efcaf9
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2023-08-06 23:25:34 -0400

    Rebuilt from scratch in order to make Prisma work

--- routes/api/shipment.ts
+++ routes/api/shipment.ts
@@ -1,6 +1,6 @@
 // API endpoint for shipping form to send data to and written to PlanetScale Database via Prisma ORM
 
-import {PrismaClient} from "../../generated/client";
+import {PrismaClient} from "../../generated/client/deno/edge.ts";
 import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { load } from "https://deno.land/std@0.197.0/dotenv/mod.ts";
 
@@ -18,17 +18,27 @@ const router = new Router();
 
 router.post("/shipment", async (context) => {
     // Extract shipment data from the request body
-    const { name, packageType, destination, email } = await context.request.body("json").value;
+    const body = context.request.body({ type: "json" });
+    const data = await body.value;
+    const { planet, hazardous, animal, fragile, oversize, description, cost, email, name } = data;
 
     // Add it to the database using Prisma
     const result = await prisma.shipment.create({
       data: {
-        name,
-        packageType,
-        destination,
-        email
+          planet,
+          hazardous,
+          animal,
+          fragile,
+          oversize,
+          description,
+          cost,
+          email,
+          name
       },
     });
 
     context.response.body = result;
 });
+
+
+
